e2ae50761019b1e6067279d5d19cd51ef9d95a74
ArtemParhom <artkievukr@ukr.net>
2023-04-11 23:00:19 +0300

    pret

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,20 +3,20 @@ import { FriendListItem } from './FriendListItem';
 import css from './FriendList.module.css';
 
 export const FriendList = ({ friends }) => {
-    
-    return (
-        <ul className={css.friendList}>
-            {friends.map(el => (
-                < FriendListItem
-                    key={el.id}
-                    avatar={el.avatar}
-                    name={el.name}
-                    isOnline={el.isOnline}
-                />
-            ))}
-        </ul>)
-}
+  return (
+    <ul className={css.friendList}>
+      {friends.map(el => (
+        <FriendListItem
+          key={el.id}
+          avatar={el.avatar}
+          name={el.name}
+          isOnline={el.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
 
 FriendList.protTypes = {
-    friends: PropTypes.array.isRequired,
+  friends: PropTypes.array.isRequired,
 };
